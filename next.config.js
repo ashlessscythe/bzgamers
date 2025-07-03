91b722824aa8cdbd82dbdf8adbfef599ce8fbc1a
ashlessscythe <alien@mbook.local>
2025-07-03 11:47:14 -0600

    secvuln low prio

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   // App Router is enabled by default in Next.js 13+
   experimental: {
     // Enable any experimental features if needed
